d1a94d75f7ab96aa4a9456fe631fff7f1b94af10
Golden-I <65423579+Golden-I@users.noreply.github.com>
2023-02-23 21:56:37 +0200

    "Part IV - Set up MongoDB and connect to application"

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const photoRoutes = require("./routes/photoRoutes");
 const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
 
 const app = express();
 app.use(express.json());
 const port = process.env.port || 3000;
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to the Unsplash API!" });
