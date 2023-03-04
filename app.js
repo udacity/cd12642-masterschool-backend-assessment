f84bff2048f5594092362b9b4e0c1dfa8fbfbc76
michalbendavid <michalbendavid770@gmail.com>
2023-03-04 17:08:44 +0200

    part V - authantication (without logout route)

--- app.js
+++ app.js
@@ -7,13 +7,17 @@ connectDB();
 const port = process.env.PORT || 3000;
 const app = express();
 
-
 //part I code
 // app.get('/', (req, res) => {
 //     res.status(200).json({message: 'Welcome to the Unsplash API!'});
 // })
 
+//add middleware to use req.body
+ app.use(express.json())
+ app.use(express.urlencoded({ extended: false }))
+
 app.use('/api/photos', require('./routes/photoRoutes.js'));
+app.use('/api/users', require('./routes/userRoutes.js'));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
